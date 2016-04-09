8b4d09609980e1ab09034bd5cf606da5a2475a26
Timon <mail@weeaboo.nl>
2016-04-09 14:19:21 +0200

    Add some tests for IntMap

--- core/src/main/java/nl/weeaboo/collections/IntMap.java
+++ core/src/main/java/nl/weeaboo/collections/IntMap.java
@@ -2,9 +2,9 @@ package nl.weeaboo.collections;
 
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
 
 /**
  * Maps ints to Objects.
@@ -15,12 +15,20 @@ public final class IntMap<V> implements Serializable {
 
 	private static final Object REMOVED = new Object();
 
-	private int[] keys; // Sorted array of keys
-	private Object[] values;
-	private int length; // First $length slots of keys are filled
-	private int size; // The number of non-removed slots in $values
+    /** Sorted array of keys */
+    private int[] keys;
 
-	private boolean containsGaps;
+    /** Associated values for each key index. Uses {@link #REMOVED} to indicate an invalid/removed slot. */
+	private Object[] values;
+
+    /** Only the first {@code length} slots contain values */
+    private int length;
+
+    /** The number of valid slots, ignoring removed slots. */
+    private int size;
+
+    /** Internal modification counter used to detect concurrent modification errors */
+    private int modCount;
 
 	public IntMap() {
 		this(8);
@@ -31,14 +39,34 @@ public final class IntMap<V> implements Serializable {
 		values = new Object[initialCapacity];
 	}
 
-	// Functions
-	private void resize(int capacity) {
+    public IntMap(Map<Integer, ? extends V> map) {
+        this(map.size());
+
+        // Add keys
+        int t = 0;
+        for (Integer key : map.keySet()) {
+            keys[t++] = key.intValue();
+        }
+        Arrays.sort(keys, 0, t);
+
+        // Add values (must be done after sorting)
+        for (int n = 0; n < t; n++) {
+            values[n] = map.get(keys[n]);
+        }
+    }
+
+    private void reserve(int capacity) {
+        if (capacity < length) {
+            throw new IllegalArgumentException(
+                    "New capacity (" + capacity + ") is smaller than the current length (" + length + ")");
+        }
+
 		int[] newKeys = new int[capacity];
 		Object[] newVals = new Object[capacity];
-		
+
 		System.arraycopy(keys, 0, newKeys, 0, length);
 		System.arraycopy(values, 0, newVals, 0, length);
-		
+
 		keys = newKeys;
 		values = newVals;
 	}
@@ -47,16 +75,20 @@ public final class IntMap<V> implements Serializable {
 		int i = Arrays.binarySearch(keys, 0, length, key);
 		if (i >= 0 && values[i] != REMOVED) {
 			@SuppressWarnings("unchecked")
-			V oldval = (V)values[i];			
+			V oldval = (V)values[i];
 			values[i] = REMOVED;
-			containsGaps = true;			
 			size--;
+            modCount++;
 			return oldval;
 		}
 		return null;
 	}
 
 	private void compact() {
+        if (size == length) {
+            return; // Already compacted
+        }
+
 		int removed = 0;
 		for (int n = 0; n < length; n++) {
 			Object val = values[n];
@@ -70,24 +102,32 @@ public final class IntMap<V> implements Serializable {
 		for (int n = length - removed; n < length; n++) {
 			values[n] = null;
 		}
-		containsGaps = false;
 		length -= removed;
+        modCount++;
 	}
 
 	public void clear() {
 		Arrays.fill(values, 0, length, null);
-		containsGaps = false;
 		length = 0;
 		size = 0;
+        modCount++;
 	}
-	
+
 	public Iterable<V> values() {
 		return new Iterable<V>() {
 			@Override
 			public Iterator<V> iterator() {
 				return new Iterator<V>() {
-					int cursor = -1;
-					
+
+                    private final int expectedModCount = modCount;
+                    private int cursor = -1;
+
+                    private void checkModCount() {
+                        if (modCount != expectedModCount) {
+                            throw new ConcurrentModificationException();
+                        }
+                    }
+
 					@Override
 					public boolean hasNext() {
 						return cursor + 1 < size;
@@ -95,40 +135,39 @@ public final class IntMap<V> implements Serializable {
 
 					@Override
 					public V next() {
+                        checkModCount();
 						return valueAt(++cursor);
 					}
 
 					@Override
 					public void remove() {
+                        checkModCount();
 						removeAt(cursor);
-					}					
+					}
 				};
 			}
 		};
 	}
-	
-	// Getters
+
 	public int[] getKeys() {
-		if (containsGaps) {
-			compact();
-		}
-		
+        compact();
+
 		int[] result = new int[length];
 		System.arraycopy(keys, 0, result, 0, length);
 		return result;
 	}
-	
+
 	public boolean containsKey(int key) {
 		int i = Arrays.binarySearch(keys, 0, length, key);
 		return i >= 0 && values[i] != REMOVED;
 	}
-	
+
 	public V get(int key) {
 		int i = Arrays.binarySearch(keys, 0, length, key);
 		if (i < 0 || values[i] == REMOVED) {
 			return null;
 		}
-		
+
 		@SuppressWarnings("unchecked")
 		V val = (V) values[i];
 		return val;
@@ -137,86 +176,75 @@ public final class IntMap<V> implements Serializable {
 	public boolean isEmpty() {
 		return size() == 0;
 	}
-	
+
 	public int size() {
 		return size;
 	}
-	
+
+    private void checkIndex(int index) {
+        compact(); // Clear removed slots
+
+        if (index >= length) {
+            throw new ArrayIndexOutOfBoundsException("Index (" + index + ") out of bounds, size=" + size);
+        }
+    }
+
 	public int keyAt(int index) {
-		if (containsGaps) {
-			compact();
-		}
-		if (index >= length) {
-			throw new ArrayIndexOutOfBoundsException(index);
-		}
-		return keys[index];
+        compact();
+        checkIndex(index);
+
+        return keys[index];
 	}
-	
+
 	public V valueAt(int index) {
-		if (containsGaps) {
-			compact();
-		}
-		if (index >= length) {
-			throw new ArrayIndexOutOfBoundsException(index);
-		}
-		
-		Object raw = values[index];
-		if (raw == REMOVED) {
-			return null;
-		}
-		
+        compact();
+        checkIndex(index);
+
 		@SuppressWarnings("unchecked")
-		V val = (V) values[index];
+        V val = (V)values[index];
 		return val;
 	}
 
-	// Setters
-	public void putAll(Map<Integer, ? extends V> map) {
-		for (Entry<Integer, ? extends V> entry : map.entrySet()) {
-			int key = entry.getKey().intValue();
-			put(key, entry.getValue());
-		}
+    public <T extends V> void putAll(Map<Integer, T> map) {
+        putAll(new IntMap<T>(map));
 	}
-	
+
 	public void putAll(IntMap<? extends V> map) {
-		for (int n = 0; n < map.length; n++) {
-			if (map.values[n] != REMOVED) {
-				@SuppressWarnings("unchecked")
-				V val = (V)map.values[n];
-				put(map.keys[n], val);
-			}
-		}
+        if (this == map) {
+            throw new IllegalArgumentException("Cannot add map to itself");
+        }
+
+        for (int n = 0; n < map.length; n++) {
+            if (map.values[n] != REMOVED) {
+                @SuppressWarnings("unchecked")
+                V val = (V)map.values[n];
+                put(map.keys[n], val);
+            }
+        }
 	}
-	
+
 	public V put(int key, V val) {
+        modCount++;
+
 		int i = Arrays.binarySearch(keys, 0, length, key);
 		if (i >= 0) {
-			// Key already exists, overwrite exiting value			
-			@SuppressWarnings("unchecked")
-			V oldval = (V)values[i];
-			if (oldval == REMOVED) {
-				oldval = null;
-				size++;
-			}
-			values[i] = val;
-			return oldval;
+			// Key already exists, overwrite exiting value
+            return doPutAtIndex(i, val);
 		}
 
 		// Find insertion index
 		i = -(i + 1);
 
 		if (i < length && values[i] == REMOVED) {
-			// Key doesn't exist, but the value following it has been deleted so
-			// we can use its slot
+            // Key doesn't exist, but the value following it has been deleted so we can use its slot
 			keys[i] = key;
 			values[i] = val;
 			size++;
 			return null; //Old value was a dummy
 		}
 
-		if (containsGaps && length >= keys.length) {
-			// Array is full, but contains gaps so try to compact in order to
-			// make room
+        if (size < length && length >= keys.length) {
+            // Array is full, but contains gaps so try to compact in order to make room
 			compact();
 
 			// Update i for the new situation
@@ -226,7 +254,7 @@ public final class IntMap<V> implements Serializable {
 
 		if (length >= keys.length) {
 			// We need more room for the new index
-			resize(length + 16);
+            reserve(length + 16);
 		}
 
 		if (i < length) {
@@ -242,42 +270,46 @@ public final class IntMap<V> implements Serializable {
 		length++;
 		return null; // Old value doesn't exist
 	}
-	
-	public boolean removeAt(int index) {
-		if (containsGaps) {
-			compact();
-		}
-		if (index >= length) {
-			throw new ArrayIndexOutOfBoundsException(index);
-		}
-		
-		Object oldval = values[index];
+
+    public V removeAt(int index) {
+        compact();
+        checkIndex(index);
+
+        modCount++;
+
+        Object oldval = values[index];
 		if (oldval == REMOVED) {
-			return false;
+            return null;
 		}
-		
+
 		values[index] = REMOVED;
-		containsGaps = true;
 		size--;
-		return true;
+
+        @SuppressWarnings("unchecked")
+        V result = (V)oldval;
+        return result;
 	}
 
 	public V putAtIndex(int index, V val) {
-		if (containsGaps) {
-			compact();
-		}
-		if (index >= length) {
-			throw new ArrayIndexOutOfBoundsException(index);
-		}
-		
-		@SuppressWarnings("unchecked")
-		V oldval = (V)values[index];
-		if (oldval == REMOVED) {
-			oldval = null;
-			size++;
-		}
-		values[index] = val;
-		return oldval;
+        compact();
+        checkIndex(index);
+
+        modCount++;
+
+        return doPutAtIndex(index, val);
+    }
+
+    private V doPutAtIndex(int index, V val) {
+        Object oldval = values[index];
+        if (oldval == REMOVED) {
+            oldval = null;
+            size++;
+        }
+        values[index] = val;
+
+        @SuppressWarnings("unchecked")
+        V result = (V)oldval;
+        return result;
 	}
-	
+
 }
---
+++ core/src/test/java/nl/weeaboo/collections/IntMapTest.java
@@ -0,0 +1,107 @@
+package nl.weeaboo.collections;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class IntMapTest {
+
+    private static final Value A = new Value();
+    private static final Value B = new Value();
+    private static final Value C = new Value();
+
+    private IntMap<Value> intMap;
+
+    @Before
+    public void before() {
+        intMap = new IntMap<Value>();
+    }
+
+    /** Add/remove a bunch of values randomly and check that the behavior matches {@link HashMap} */
+    @Test
+    public void addRemoveRandom() {
+        Random random = new Random(12345);
+
+        Map<Integer, Value> hashMap = new HashMap<Integer, Value>();
+        for (int n = 0; n < 10000; n++) {
+            int key = random.nextInt(200) - 100;
+            Value val = new Value();
+
+            Object oldIntMapVal;
+            Object oldHashMapVal;
+            if (random.nextDouble() <= 0.25) {
+                oldIntMapVal = intMap.remove(key);
+                oldHashMapVal = hashMap.remove(key);
+            } else {
+                oldIntMapVal = intMap.put(key, val);
+                oldHashMapVal = hashMap.put(key, val);
+            }
+
+            // Check that intmap behavior matches hashmap
+            Assert.assertEquals(hashMap.size(), intMap.size());
+            Assert.assertEquals("Incorrect value for key " + key, hashMap.get(key), intMap.get(key));
+            Assert.assertEquals(oldHashMapVal, oldIntMapVal);
+        }
+    }
+
+    /** Test iterating over values */
+    @Test
+    public void iterateValues() {
+        intMap.put(3, B);
+        intMap.put(-1, A);
+        intMap.put(200, C);
+
+        List<Value> values = new ArrayList<Value>();
+        for (Value value : intMap.values()) {
+            values.add(value);
+        }
+
+        Assert.assertArrayEquals(new Object[] { A, B, C }, values.toArray());
+    }
+
+    /** Access slots (key/value) by index */
+    @Test
+    public void accessByIndex() {
+        intMap.put(3, B);
+        intMap.put(-1, A);
+        intMap.put(200, C);
+
+        Assert.assertEquals(3, intMap.size());
+
+        Assert.assertEquals(-1, intMap.keyAt(0));
+        Assert.assertEquals(A, intMap.valueAt(0));
+
+        Assert.assertEquals(3, intMap.keyAt(1));
+        Assert.assertEquals(B, intMap.valueAt(1));
+
+        Assert.assertEquals(200, intMap.keyAt(2));
+        Assert.assertEquals(C, intMap.valueAt(2));
+
+        // Remove the middle slot
+        Assert.assertEquals(B, intMap.removeAt(1));
+        Assert.assertEquals(2, intMap.size());
+
+        Assert.assertEquals(-1, intMap.keyAt(0));
+        Assert.assertEquals(A, intMap.valueAt(0));
+
+        Assert.assertEquals(200, intMap.keyAt(1));
+        Assert.assertEquals(C, intMap.valueAt(1));
+
+        // Overwrite a slot's value
+        Assert.assertEquals(C, intMap.putAtIndex(1, B));
+        Assert.assertEquals(2, intMap.size());
+
+        Assert.assertEquals(200, intMap.keyAt(1));
+        Assert.assertEquals(B, intMap.valueAt(1));
+    }
+
+    private static final class Value {
+    }
+
+}
