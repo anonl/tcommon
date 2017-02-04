d40171b8ec7bd3f9143f2b6ad457db0d1dac0a90
Timon <mail@weeaboo.nl>
2017-02-04 12:42:27 +0100

    Add test helper methods for Dim2D

---
+++ test/src/main/java/nl/weeaboo/gdx/test/DimAssert.java
@@ -0,0 +1,20 @@
+package nl.weeaboo.gdx.test;
+
+import org.junit.Assert;
+
+import nl.weeaboo.common.Dim2D;
+
+public final class DimAssert {
+
+    private DimAssert() {
+    }
+
+    /**
+     * Checks if two dimensions are equal, allowing up to a small {@code epsilon} difference.
+     */
+    public static void assertEquals(Dim2D expected, Dim2D actual, double epsilon) {
+        Assert.assertEquals("Invalid w: " + actual, expected.w, actual.w, epsilon);
+        Assert.assertEquals("Invalid h: " + actual, expected.h, actual.h, epsilon);
+    }
+
+}
